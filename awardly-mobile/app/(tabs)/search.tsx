58268b6498df79584e1c7c3d4f18379a98d245ce
Bento Guilherme <gbento555@gmail.com>
2026-05-23 11:23:46 -0300

    projeto renovado pra sdk 54

---
+++ awardly-mobile/app/(tabs)/search.tsx
@@ -0,0 +1,10 @@
+// search.tsx
+import { View, Text } from 'react-native';
+
+export default function Search() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Pesquisa</Text>
+    </View>
+  );
+}
